a83d0a64b0ff694c2e2b727dda2a436490a50dfe
Leon Kountouras <koundouras@gmail.com>
2024-02-19 19:08:21 +0200

    Update memory.js

--- memory.js
+++ memory.js
@@ -5,7 +5,7 @@ const os = require('os');
 os.totalmem();
 os.freemem(); 
 
-const totalmem = () => return `Total Memory: ${os.totalmem()}`;
+const totalmem = () => `Total Memory: ${os.totalmem()}`;
 // Google for how to convert bytes into MBs or GBs
 
 module.exports = {
