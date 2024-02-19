8c2d42f313d16747fb804338386e128e66b9581e
Leon Kountouras <koundouras@gmail.com>
2024-02-19 18:54:25 +0200

    Create memory.js

---
+++ memory.js
@@ -0,0 +1,11 @@
+// TODO: Improve the code below to show the memory in MBs or GBs
+
+os.totalmem();
+os.freemem(); 
+
+const totalmem = () => return `Total Memory: ${os.totalmem()}`;
+// Google for how to convert bytes into MBs or GBs
+
+module.exports = {
+  totalmem
+}
