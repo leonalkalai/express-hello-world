94eaf5c32752894582e08128ec51d2a0bacf8ed3
Leon Kountouras <koundouras@gmail.com>
2024-02-20 20:50:37 +0200

    Create hardware.js

---
+++ hardware.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+const serverMachine = () => os.machine();
+const osVersion = () => os.version();
+const osUptime = () => os.uptime();
+const userInfo = () => os.userInfo();
+const osType = () => os.type();
+const osPlatform = () => os.platform();
+const serverCpus = () => os.cpus();
+
+module.exports = {
+ serverMachine,
+ osVersion,
+ osUptime,
+ userInfo,
+ osType,
+ osPlatform,
+ serverCpus
+};
