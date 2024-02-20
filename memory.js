e226dd4acfece939f5d192e4b67cf06ecaa1ea6b
Leon Kountouras <koundouras@gmail.com>
2024-02-20 10:33:33 +0200

    Update memory.js

--- memory.js
+++ memory.js
@@ -15,23 +15,25 @@ X = os.totalmem()  *  (1000 ** 3 );
 
 const os = require('os');
 
-//os.freemem(); 
-
-const { displayTotalMemory } = require('./app.js');
-
 const totalMem = () => os.totalmem();
 const bytesToMegabytes = () => os.totalmem() / (1000 ** 2);
 // Google for how to convert bytes into MBs or GBs
 const bytesToGigabytes = () => os.totalmem() / (1000 ** 3);
+
+module.exports = {
+  totalMem,
+  bytesToMegabytes,
+  bytesToGigabytes,
+}
+//os.freemem(); 
+
+const { displayTotalMemory } = require('./app.js');
 
 const displayMemoryBytes = displayTotalMemory(totalMem);
 const displayMemoryMegaBytes = displayTotalMemory(bytesToMegabytes);
 const displayMemoryGigaBytes = displayTotalMemory(bytesToGigabytes);
   
 module.exports = {
-  totalMem,
-  bytesToMegabytes,
-  bytesToGigabytes,
   displayMemoryBytes,
   displayMemoryMegaBytes,
   displayMemoryGigaBytes
