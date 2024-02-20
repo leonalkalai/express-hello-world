5ab257e0e4cbbd5bc0098cc03b64e15fe448adb3
Leon Kountouras <koundouras@gmail.com>
2024-02-20 11:40:57 +0200

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,6 @@
+//define memory calculations
+
+const os = require('os');
+const totalMem = () => os.totalmem();
+const bytesToMegabytes = () => os.totalmem() / (1000 ** 2);
+const bytesToGigabytes = () => os.totalmem() / (1000 ** 3);
