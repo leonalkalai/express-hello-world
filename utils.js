a4a9cce72cebd0dee2425d4b6957b4ac172dad01
Leon Kountouras <koundouras@gmail.com>
2024-02-20 12:02:15 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,11 +2,14 @@
 
 const os = require('os');
 
+const numberToInteger = (n) => Math.round(parseFloat(n));
+
 const totalMem = () => os.totalmem();
 const bytesToMegabytes = () => os.totalmem() / (1000 ** 2);
 const bytesToGigabytes = () => os.totalmem() / (1000 ** 3);
 
 module.exports = {
+  numberToInteger,
   totalMem,
   bytesToMegabytes,
   bytesToGigabytes
