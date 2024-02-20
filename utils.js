7bfde77a0a4774679f57c89361f46175117205f4
Leon Kountouras <koundouras@gmail.com>
2024-02-20 11:41:25 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,6 +1,13 @@
 //define memory calculations
 
 const os = require('os');
+
 const totalMem = () => os.totalmem();
 const bytesToMegabytes = () => os.totalmem() / (1000 ** 2);
 const bytesToGigabytes = () => os.totalmem() / (1000 ** 3);
+
+module.exports = {
+  totalMem,
+  bytesToMegabytes,
+  bytesToGigabytes
+};
