cb18f2cef5634eae70950815929ee34a1be66d66
Leon Kountouras <koundouras@gmail.com>
2024-02-21 17:15:00 +0200

    Update hardware.js

--- hardware.js
+++ hardware.js
@@ -6,7 +6,38 @@ const osUptime = () => os.uptime();
 const userName = () => os.userInfo().username;
 const osType = () => os.type();
 const osPlatform = () => os.platform();
-const serverCpus = () => os.cpus();
+/* longhand
+const serverCpus = () => os.cpus().map((cpu, index) => {
+  return {
+    index: index,
+    model: cpu.model,
+    speed: cpu.speed,
+    times: cpu.times.map(time => {
+      return {
+        user: time.user,
+        nice: time.nice,
+        sys: time.sys,
+        idle: time.idle,
+        irq: time.irq
+      };
+    })
+  };
+});
+*/
+
+// shorthand
+const serverCpus = () => os.cpus().map((cpu, index) => ({
+    index: index,
+    model: cpu.model,
+    speed: cpu.speed,
+    times: cpu.times.map(time => ({
+        user: time.user,
+        nice: time.nice,
+        sys: time.sys,
+        idle: time.idle,
+        irq: time.irq
+    }))
+}));
 
 module.exports = {
  serverMachine,
