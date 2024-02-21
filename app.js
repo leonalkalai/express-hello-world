688747c0b0cf9235f467c722e5f3abe3b5270937
Leon Kountouras <koundouras@gmail.com>
2024-02-21 16:20:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ const hardwareprops = {
 
 const displayHardwareInfo = (prop) => {
  const result = hardwareprops[prop](); 
-  return `<p>${prop}: <span class="bytes">${JSON.stringify(result)}</span></p>`;
+  return `<p>${prop}: <span class="bytes">${typeof result === 'object' ? JSON.stringify(result) : result}</span></p>`;
 };
 
 const servermachine = displayHardwareInfo('serverMachine');
