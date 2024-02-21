74b20b113a182b0ab6da09af79385b0934dec668
Leon Kountouras <koundouras@gmail.com>
2024-02-21 13:18:24 +0200

    Update hardware.js

--- hardware.js
+++ hardware.js
@@ -3,7 +3,7 @@ const os = require('os');
 const serverMachine = () => os.machine();
 const osVersion = () => os.version();
 const osUptime = () => os.uptime();
-const userInfo = () => os.userInfo();
+const userInfo = () => os.userInfo().username;
 const osType = () => os.type();
 const osPlatform = () => os.platform();
 const serverCpus = () => os.cpus();
